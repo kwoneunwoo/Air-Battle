518bb8e4be078648764cd252b0a244ec68026637
kwoneunwoo <kwoneunwoo07@gmail.com>
2023-11-26 23:54:26 +0900

    bugfix: UnboundLocalError

--- screens/ending.py
+++ screens/ending.py
@@ -40,14 +40,14 @@ class EndingScreen:
             gameover_text = self.font.render('GAME OVER', True, config.Color.red)
             reset_text = self.small_text.render('Press r to restart', True, config.Color.white)
 
+            # 텍스트의 높이와 y값을 계산합니다
+            info_height = gameover_text.get_height() + reset_text.get_height() + text_margin
+            info_y = config.screen_height/2 - info_height/2
+
             # 검정색 반투명 화면의 크기를 설정합니다
             background = pygame.Surface((config.screen_width, info_height+bg_margin))
             background.fill(config.Color.black)
             background.set_alpha(10)
-
-            # 텍스트의 높이와 y값을 계산합니다
-            info_height = gameover_text.get_height() + reset_text.get_height() + text_margin
-            info_y = config.screen_height/2 - info_height/2
 
             # 텍스트의 x값을 중앙으로 설정합니다
             gameover_text_x = config.screen_width/2-gameover_text.get_width()/2
